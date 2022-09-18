d71e537741f6587988084547a2a01dd7c20306df
ronwho <codingmonkron@gmail.com>
2022-09-18 17:19:40 -0400

    added bird.py and repitle.py

---
+++ bird.py
@@ -0,0 +1,6 @@
+def main():
+    print('the bird goes chirp! chirp! chrip!')
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ reptile.py
@@ -0,0 +1,6 @@
+def main():
+    print('the reptile goes hiss! hiss! hiss!')
+    return
+
+if __name__ == '__main__':
+    main()
